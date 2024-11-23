8a559b01d9dbf6b0ce46ef45094c4d5c139568f1
Adrián Enríquez <adri95enriquez@gmail.com>
2024-11-23 22:58:14 +0100

    Scaffold CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
 import faviconsInjectPlugin from 'vite-plugin-favicons-inject'
 
 export default defineConfig({
   plugins: [faviconsInjectPlugin('./assets/logo.svg')],
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
 })
